3d6e78fff4535e88a3cdb225c9c65b94eefc3871
sh940701 <visioner2168@gmail.com>
2024-03-12 08:00:56 +0900

    feat: add article dto

---
+++ dto/article.py
@@ -0,0 +1,29 @@
+class ArticleDTO:
+    def __init__(self, _id, topic, author, title, date, is_blind):
+        self._id = _id
+        self.topic = topic
+        self.author = author
+        self.title = title
+        self.date = date
+        self.is_blind = is_blind
+
+    def to_dict(self):
+        return {
+            "_id": self._id,
+            "topic": self.topic,
+            "author": self.author,
+            "title": self.title,
+            "date": self.date,
+            "is_blind": self.is_blind
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return ArticleDTO(
+            _id=data.get("id"),
+            topic=data.get("topic"),
+            author=data.get("author"),
+            title=data.get("title"),
+            date=data.get("date"),
+            is_blind=data.get("is_blind")
+        )
--- services/articles.py
+++ services/articles.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from db import articles_collection
+from dto.article import ArticleDTO
 
 # html 파일이 있는 folder path 정의
 articles_blueprint = Blueprint("articles_blueprint", __name__, template_folder="../templates/articles")
@@ -7,7 +8,22 @@ articles_blueprint = Blueprint("articles_blueprint", __name__, template_folder="
 
 @articles_blueprint.route("/")
 def get_all_articles():
-    return render_template('article_list.html')
+    # list_of_articles = [
+    #     {"topic": "good", "author": "홍길동", "title": "칭찬합니다.", "date": "2024-03-11", "is_blind": False},
+    #     {"topic": "bad", "author": "양장피", "title": "싫습니다.", "date": "2024-03-11", "is_blind": True},
+    #     {"topic": "bad", "author": "김꺽정", "title": "싫습니다.", "date": "2024-03-10", "is_blind": True},
+    #     {"topic": "good", "author": "임꺽정", "title": "칭찬합니다.", "date": "2024-03-09", "is_blind": False}
+    # ]
+
+    list_of_articles = list(articles_collection.find())
+
+    # ObjectId 를 문자열로 변환
+    for article in list_of_articles:
+        article["_id"] = str(article["_id"])
+
+    articles_object = [ArticleDTO.from_dict(article_dict) for article_dict in list_of_articles]
+
+    return render_template('article_list.html', articles=articles_object)
 
 
 @articles_blueprint.route("/", methods=["POST"])
