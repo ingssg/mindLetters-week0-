716ea1303eb8f53781e5f9c77e7a058e8f1fa8c4
sh940701 <visioner2168@gmail.com>
2024-03-11 21:46:12 +0900

    update: separate list_all, list_one article

--- services/articles.py
+++ services/articles.py
@@ -6,4 +6,9 @@ articles_blueprint = Blueprint("articles_blueprint", __name__, template_folder="
 
 @articles_blueprint.route("/")
 def get_all_articles():
-    return render_template('index.html')
+    return render_template('article_list.html')
+
+
+@articles_blueprint.route("/<string:id>")
+def get_one_articles(id):
+    return render_template('article_detail.html', id=id)
