c0934c3092f45e8134240c0764b5a7e3881d6dd5
Inseok Kim <js2121good@gmail.com>
2024-03-12 18:46:33 +0900

    feat: signup.html db연동, signin.html db연동된 id로 로그인

--- services/users.py
+++ services/users.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify, redirect, url_for, request
-from db import articles_collection
+from db import articles_collection, users_collection
+from datetime import datetime
 import jwt
 import os
 import hashlib
@@ -12,10 +13,10 @@ users_blueprint = Blueprint("users_blueprint", __name__, template_folder="../tem
 def generate_salt():
     return os.urandom(16).hex()
 
-# 비밀번호 암호화(솔팅, 스트레칭)
+# 비밀번호 솔팅, 키 스트레칭
 def hash_password(password, salt):
     hashed_password = password
-    for _ in range(1000):
+    for _ in range(1328): # 반복 횟수 1000~10000 사이 값 (서버 과부하 방지 및 보안 고려)
         hashed_password = hashlib.sha256((hashed_password + salt).encode()).hexdigest()
     return hashed_password
 
@@ -27,8 +28,25 @@ def signin():
 @users_blueprint.route("/signin", methods=["POST"])
 def signin_user():
     # 로그인 기능 구현
-    return
+    user = {
+    'id': request.form.get('id', ''),
+    'password': request.form.get('password', ''),
+    }
 
+    print(user['id'] + '====' + user['password'])
+
+    # 빈 인풋 체크
+    if not user['id'] or not user['password']:
+        return render_template('signin.html', error="모든 필드를 입력해주세요.")
+
+    user_info = users_collection.find_one({"id": user['id']})
+    salt = user_info['salt']
+
+    print(user['password'] + '====' + user_info['hashed_password'])
+    if(hash_password(user['password'], salt) != user_info['hashed_password']):
+        return render_template('signin.html', error="로그인 정보가 올바르지 않습니다.")
+
+    return render_template('articles/article_list.html')
 
 @users_blueprint.route("/signup")
 def signup():
@@ -40,23 +58,30 @@ def create_user():
     # 회원 가입 기능 구현
     isSignUp = False
     salt = generate_salt()
+
     user = {
-        'id': request.form['id'],
-        'hashed_password': hash_password(request.form['password'], salt),
-        'nickname': request.form['nickname'],
+    'id': request.form.get('id', ''),
+    'hashed_password': '',
+    'nickname': request.form.get('nickname', ''),
+    'salt': salt,
+    'createdAt': datetime.now(),
     }
+
     # 회원 가입 로직
     # 빈 인풋 체크
-    if not user['id'] or not request.form['password'] or not user['nickname']:
+    if not user['id'] or not request.form.get('password', '') or not user['nickname']:
         return render_template('signup.html', error="모든 필드를 입력해주세요.")
     
     # 비밀번호 길이 확인
-    if len(request.form['password']) < 8:
+    if len(user['password']) < 8:
         return render_template('signup.html', error="비밀번호는 최소 8자 이상이어야 합니다.")
-    
+
+    user['hashed_password'] = hash_password(request.form['password'], salt)
+
+
     isSignUp = True
     print(user)
     # DB에 유저 정보 추가
-    # articles_collection.insert_one(user)
+    users_collection.insert_one(user)
     
     return render_template('signin.html', isSignUp=isSignUp)
